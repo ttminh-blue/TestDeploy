0075cf7f30db956b2624a708eda076b9d14601a2
Minh Tran <minhtran@ankvn.com>
2026-05-19 14:51:43 +0700

    Convert to Number

--- backend/app.ts
+++ backend/app.ts
@@ -13,7 +13,7 @@ import debug from 'debug';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = process.env.DEFAULT_PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
