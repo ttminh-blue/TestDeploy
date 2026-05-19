d5d9232d7afd867228cacc32e1885c781c116168
Minh Tran <minhtran@ankvn.com>
2026-05-19 14:16:22 +0700

    Fix deployment

--- backend/app.ts
+++ backend/app.ts
@@ -13,7 +13,7 @@ import debug from 'debug';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = process.env.DEFAULT_PORT;
+const port = process.env.DEFAULT_PORT || 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
@@ -59,7 +59,7 @@ app.get('/ping', (req: express.Request, res: express.Response) => {
   res.status(200).send('pong');
 });
 
-export default server.listen(port, () => {
+export default server.listen(port, '0.0.0.0',() => {
   routes.forEach((route: CommonRoutesConfig) => {
     debugLog(`Routes configured for ${route.getName()}`)
   });
